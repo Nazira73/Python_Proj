2541cc37a6479f2ce4517bf210728a9b85393c89
Nazira shaikh <38313110+Nazira73@users.noreply.github.com>
2024-07-05 11:27:55 +0530

    comments added

--- string_functions.py
+++ string_functions.py
@@ -1,8 +1,11 @@
+# reverse of string
 def reverse(x):
     return x[::-1]
 
+# pallindrome test
 def pallindrome(x):
     return x.lower() == reverse(x.lower())
 
+# case conversion to lowercase
 def convert_lower(x):
     return x.lower()
